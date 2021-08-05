edd58161d448b5d41a5b7c5f6fd785569eff4528
dung19951 <kingtrungdung@gmail.com>
2021-08-05 14:50:59 +0700

    migration_product

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FoodsController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,9 +23,9 @@ Route::prefix('home')->group(function (){
     Route::get('login',[LoginController::class,'index'])->name('home.showLogin');
     Route::post('login',[LoginController::class,'login'])->name('home.login');
     Route::get('index',[FoodsController::class,'index'])->name('home.index');
+    Route::get('register',[RegisterController::class,'create'])->name('register.index');
+    Route::post('register',[RegisterController::class,'store'])->name('register.store');
 });
-
-Route::prefix('home')->group(function (){
-    Route::get('/register',[\App\Http\Controllers\RegisterController::class,'create'])->name('register.index');
-    Route::post('/register',[\App\Http\Controllers\RegisterController::class,'store'])->name('register.store');
+Route::get('test',function (){
+    return view('backend.master');
 });
