17d5bd113242c6bba6c3a568eb521a66f03c1015
nguyenngoc-it <nguyenngocit.na@gmail.com>
2021-08-05 15:45:02 +0700

    ok

--- app/Http/Controllers/FoodsController.php
+++ app/Http/Controllers/FoodsController.php
@@ -9,5 +9,16 @@ class FoodsController extends Controller
     public function index()
     {
         return view('backend.foods.list');
-  }
+    }
+
+    public function edit($id)
+    {
+
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
